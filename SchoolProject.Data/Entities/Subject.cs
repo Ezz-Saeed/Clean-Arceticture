72d08bd7f2b89b59e7356b0b2409fe98227f1bbf
U-CAIRO\Moataz.Abdelmegeed <ezzsaid299@gmail.com>
2025-06-20 19:08:32 +0300

    initial commit

---
+++ SchoolProject.Data/Entities/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject.Data;
+
+public class Subject
+{
+public Subject()
+        {
+            StudentsSubjects = new HashSet<StudentSubject>();
+            DepartmetsSubjects = new HashSet<DepartmetSubject>();
+        }
+        [Key]
+        public int SubID { get; set; }
+        [StringLength(500)]
+        public string SubjectName { get; set; }
+        public DateTime Period { get; set; }
+        public virtual ICollection<StudentSubject> StudentsSubjects { get; set; }
+        public virtual ICollection<DepartmetSubject> DepartmetsSubjects { get; set; }
+}
---
+++ SchoolProject.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data;
+
+namespace SchoolProject.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+    public virtual DbSet<Department> Departments { get; set; }
+    public virtual DbSet<Subject> Subjects { get; set; }
+}
