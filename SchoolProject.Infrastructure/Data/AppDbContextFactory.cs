0a5ede6ffb101cb3764edb4851620f35bc40b009
Ezz Saeed <ezzsaid299@gmail.com>
2025-06-21 19:08:22 +0300

    Configuring Sqlite db provider

---
+++ SchoolProject.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SchoolProject.Infrastructure;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            // Update the path if your database file is elsewhere
+            optionsBuilder.UseSqlite("Data Source=SchoolProject.db");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
---
+++ SchoolProject.Infrastructure/Migrations/20250621155617_InitialCreate.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DName = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    SubID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SubjectName = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Period = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.SubID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    DID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudID);
+                    table.ForeignKey(
+                        name: "FK_Students_Departments_DID",
+                        column: x => x.DID,
+                        principalTable: "Departments",
+                        principalColumn: "DID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmetSubject",
+                columns: table => new
+                {
+                    DeptSubID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DID = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmetSubject", x => x.DeptSubID);
+                    table.ForeignKey(
+                        name: "FK_DepartmetSubject_Departments_DID",
+                        column: x => x.DID,
+                        principalTable: "Departments",
+                        principalColumn: "DID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmetSubject_Subjects_SubID",
+                        column: x => x.SubID,
+                        principalTable: "Subjects",
+                        principalColumn: "SubID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentSubject",
+                columns: table => new
+                {
+                    StudSubID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudID = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubject", x => x.StudSubID);
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Students_StudID",
+                        column: x => x.StudID,
+                        principalTable: "Students",
+                        principalColumn: "StudID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentSubject_Subjects_SubID",
+                        column: x => x.SubID,
+                        principalTable: "Subjects",
+                        principalColumn: "SubID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmetSubject_DID",
+                table: "DepartmetSubject",
+                column: "DID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmetSubject_SubID",
+                table: "DepartmetSubject",
+                column: "SubID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_DID",
+                table: "Students",
+                column: "DID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubject_StudID",
+                table: "StudentSubject",
+                column: "StudID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubject_SubID",
+                table: "StudentSubject",
+                column: "SubID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmetSubject");
+
+            migrationBuilder.DropTable(
+                name: "StudentSubject");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
