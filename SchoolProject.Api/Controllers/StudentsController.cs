d9f033a1b192b558ff4edf29c154a1f06bf25d20
Ezz Saeed <ezzsaid299@gmail.com>
2025-06-29 19:38:45 +0300

    Customizing routing

--- SchoolProject.Api/Controllers/StudentsController.cs
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -3,21 +3,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SchoolProject.Core;
 using SchoolProject.Core.Features.Students.Queries.Models;
+using SchoolProject.Data.AppMetaData;
 
 namespace MyApp.Namespace
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class StudentsController(IMediator _mediator) : ControllerBase
     {
-        [HttpGet("/students/list")]
+        [HttpGet(Router.StudentRouting.List)]
         public async Task<IActionResult> GetStudentsAsync()
         {
             var response = await _mediator.Send(new GetNewStudentListQuery());
             return Ok(response);
         }
 
-        [HttpGet("/student/{id}")]
+        [HttpGet(Router.StudentRouting.GetById)]
         public async Task<IActionResult> GetStudentByIdAsync(int id)
         {
             var response = await _mediator.Send(new GetStudentByIdQuery(id));
