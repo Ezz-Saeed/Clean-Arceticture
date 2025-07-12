bd7985c2b9d546a63bb184e38917e7ce30f896ab
Ezz Saeed <ezzsaid299@gmail.com>
2025-07-12 14:43:03 +0300

    [200~ Handle_End_Response_With_Custom_Response

--- SchoolProject.Api/Controllers/StudentsController.cs
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Api.Controllers;
 using SchoolProject.Core;
 using SchoolProject.Core.Features.Students.Commands.Models;
 using SchoolProject.Core.Features.Students.Queries.Models;
@@ -10,27 +11,27 @@ namespace MyApp.Namespace
 {
     //[Route("api/[controller]")]
     [ApiController]
-    public class StudentsController(IMediator _mediator) : ControllerBase
+    public class StudentsController : AppControllerBase
     {
         [HttpGet(Router.StudentRouting.List)]
         public async Task<IActionResult> GetStudentsAsync()
         {
-            var response = await _mediator.Send(new GetNewStudentListQuery());
+            var response = await Mediator.Send(new GetNewStudentListQuery());
             return Ok(response);
         }
 
         [HttpGet(Router.StudentRouting.GetById)]
         public async Task<IActionResult> GetStudentByIdAsync(int id)
         {
-            var response = await _mediator.Send(new GetStudentByIdQuery(id));
-            return Ok(response);
+            var response = await Mediator.Send(new GetStudentByIdQuery(id));
+            return NewResult(response);
         }
 
         [HttpPost(Router.StudentRouting.Create)]
         public async Task<IActionResult> Create(AddStudentCommand command)
         {
-            var response = _mediator.Send(command);
-            return Ok(response);
+            var response = await Mediator.Send(command);
+            return NewResult(response);
         }
     }
 }
