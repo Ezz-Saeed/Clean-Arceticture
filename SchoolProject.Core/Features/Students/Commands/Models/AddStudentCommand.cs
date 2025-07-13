5f487c10e8aa2330e93d5a307daae342ca2cdc8d
Ezz Saeed <ezzsaid299@gmail.com>
2025-07-13 17:49:10 +0300

    Validation behavior

--- SchoolProject.Core/Features/Students/Commands/Models/AddStudentCommand.cs
+++ SchoolProject.Core/Features/Students/Commands/Models/AddStudentCommand.cs
@@ -1,19 +1,13 @@
 ﻿using MediatR;
 using SchoolProject.Core.Basis;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SchoolProject.Core.Features.Students.Commands.Models
 {
     public class AddStudentCommand : IRequest<Response<string>>
     {
-        [Required]
+        //[Required]
         public string Name { get; set; }
-        [Required]
+        //[Required]
         public string Address { get; set; }
         public string Phone { get; set; }
         public int DepartmentId { get; set; }
---
+++ SchoolProject.Core/Features/Students/Commands/Validators/AddStudentValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using SchoolProject.Core.Features.Students.Commands.Models;
+
+namespace SchoolProject.Core.Features.Students.Commands.Validators
+{
+    public class AddStudentValidator : AbstractValidator<AddStudentCommand>
+    {
+        #region ctors
+        public AddStudentValidator()
+        {
+            ApplyValidationRules();
+        }
+        #endregion
+
+        #region Actions
+        public void ApplyValidationRules()
+        {
+            RuleFor(s => s.Name).NotEmpty().WithMessage("{ProperyName} is required!")
+                .NotNull()
+                .MaximumLength(10).WithMessage("Max length for {ProperyName} is 10 characters!");
+
+            RuleFor(s => s.Address).NotEmpty().WithMessage("{ProperyName} is required!")
+                .NotNull()
+                .MaximumLength(10).WithMessage("Max length for {ProperyName} is 10 characters!");
+        }
+        #endregion
+    }
+}
